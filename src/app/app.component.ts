db5cc4431ea325e7773af89ee91e7a67339707d1
lordkroos12 <joroo.23@abv.bg>
2024-04-28 01:08:38 +0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,63 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+type Note = {
+  title: string,
+  content: string
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, FormsModule,CommonModule]
 })
 export class AppComponent {
-  title = 'angular-w3';
+  toggleButtons(index: number) {
+    this.showButtons = !this.showButtons;
+    this.currIndex = index;
+  }
+  editNote() {
+    this.editIndex = this.currIndex;
+    this.currTitle = this.notes[this.currIndex].title
+    this.currContent = this.notes[this.currIndex].content;
+  }
+  removeNote() {
+    this.notes.splice(this.currIndex, 1);
+  }
+
+  notes: Note[] = [];
+  public currTitle = '';
+  public currContent = '';
+  public showButtons = false;
+  public currIndex = 0;
+  public editIndex = -1;
+
+  saveNote() {
+    if (this.currTitle.length < 5 || this.currContent.length < 7)
+      {
+        return;
+      }
+    if (this.editIndex >= 0) {
+      this.notes[this.editIndex].title = this.currTitle
+      this.notes[this.editIndex].content = this.currContent
+    }
+    else {
+      let note: Note = {
+        title: this.currTitle,
+        content: this.currContent
+      }
+      this.notes.push(note);
+    }
+
+    this.currContent = "";
+    this.currTitle = "";
+    this.showButtons = false;
+    this.editIndex = -1;
+  }
+
+
 }
